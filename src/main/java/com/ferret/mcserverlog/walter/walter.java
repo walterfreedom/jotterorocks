8cf4ac7c9317462c7f8cbf52a382f862fe9355a4
Jottero <lookslikecat@gmail.com>
2021-11-26 23:50:23 +0300

    feret

--- src/main/java/com/ferret/mcserverlog/walter/walter.java
+++ src/main/java/com/ferret/mcserverlog/walter/walter.java
@@ -1,13 +1,30 @@
 package com.ferret.mcserverlog.walter;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+@Controller
 public class walter {
+
     @GetMapping("/walter")
-    public String getWalter(){
-        return "walter";
+    public String getWalter(Model theModel) throws FileNotFoundException {
+        //😎
+        String data = "";
+        File file = new File("/home/opc/paperserver/logs/latest.log");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine())
+            data = data+"\n"+sc.nextLine();
+        data= data.replaceAll("\\d*\\.\\d*\\.\\d*\\.\\d*", "[REDACTED]");
+        data= data.replaceAll("\\[\\d*:\\d*:\\d*\\]*", "\n \n");
+
+        theModel.addAttribute("verycoolattribute", data);
+        return "pog";
     }
     @GetMapping("/ferret")
     public String getFerret(){
