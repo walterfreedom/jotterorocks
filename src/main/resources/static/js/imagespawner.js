a50747264b3f4c3c1a82adf6cbcd08cade993247
Jottero <lookslikecat@gmail.com>
2021-11-28 13:30:47 +0300

    pickle

--- src/main/resources/static/js/imagespawner.js
+++ src/main/resources/static/js/imagespawner.js
@@ -1,5 +1,7 @@
 function imagespawner(){
-    var pickle = document.createElement("img");
+    var pickle = new Image();
     pickle.src="https://www.vhv.rs/dpng/d/409-4090710_rick-and-morty-pickle-rick-png-clipart-pickle.png";
+    pickle.height=70
+    pickle.width=70
     document.body.appendChild(pickle);
 }
